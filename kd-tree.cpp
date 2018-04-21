5cb79ee4ff2d045ba9b4ee4a3a06dc223ee8dba5
udjindal <jindaludit23@gmail.com>
2018-04-22 04:35:13 +0530

    Build kd-tree working

--- kd-tree.cpp
+++ kd-tree.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 const int k = 2;
 int n;
+#define COUNT 10
+
 // A structure to represent node of kd tree
 struct Node
 {
@@ -10,7 +12,46 @@ struct Node
     Node *left, *right;
 };
 
-// A method to create a node of K D tree
+
+bool arePointsSame(int point1[], int point2[]) {
+    // Compare individual pointinate values
+    for (int i = 0; i < k; ++i)
+        if (point1[i] != point2[i])
+            return false;
+
+    return true;
+}
+
+void print2DUtil(Node *root, int space)
+{
+    // Base case
+    if (root == NULL)
+        return;
+
+    // Increase distance between levels
+    space += COUNT;
+
+    // Process right child first
+    print2DUtil(root->right, space);
+
+    // Print current node after space
+    // count
+    printf("\n");
+    for (int i = COUNT; i < space; i++)
+        printf(" ");
+    printf("%f %f\n", root->point[0], root->point[1]);
+
+    // Process left child
+    print2DUtil(root->left, space);
+}
+
+// Wrapper over print2DUtil()
+void print2D(Node *root)
+{
+   // Pass initial space count as 0
+   print2DUtil(root, 0);
+}
+
 Node* newNode(int arr[]) {
     Node* temp = new Node;
     for (int i=0; i < k; i++)
@@ -31,20 +72,25 @@ Node* findMedian(Node *start, Node *end, int curr_dim) {
     if(end <= start)
         return NULL;
     if(end == start + 1)
-        return min(end, start);
-    Node *p, *store, *md = start + (end - start) / 2;
+        return start;
+    Node *p, *store, *md;
+    if((end - start)%2) {
+        md = start + (end - start)/2;
+    }
+    else {
+        md = start + (end - start)/2 - 1;
+    }
     double pivot;
     while(true) {
-        pivot = md->point[curr_dim];
 
-        swap(md, end -1);
+        pivot = (end-1)->point[curr_dim];
         for (store = p = start; p < end; p++) {
             if (p->point[curr_dim] < pivot) {
-                if (p != store)
-                    swap(p, store);
+                swap(p, store);
                 store++;
             }
         }
+        swap(store, end - 1);
         if (store->point[curr_dim] == md->point[curr_dim])
             return md;
 
@@ -56,37 +102,31 @@ Node* findMedian(Node *start, Node *end, int curr_dim) {
 }
 
 Node* buildKDtreeRec(Node *t, int len, int i, int dim) {
-    Node *med;
-    if(!len)
-        return 0;
-    med = findMedian(t, t+len, i);
+    Node *med = new Node;
+    Node *med2;
+    if(len == 1) {
+        return t;
+    }
+    med2 = findMedian(t, t + len, i);
+    med->point[0] = med2->point[0];
+    med->point[1] = med2->point[1];
     i = (i+1)%dim;
-    med->left = buildKDtreeRec(t, med - t, i, dim);
-    med->right = buildKDtreeRec(med+1, (t+len) - (med+1), i, dim);
+    med->left = buildKDtreeRec(t, med2 + 1 - t, i, dim);
+    med->right = buildKDtreeRec(med2 + 1, (t + len) - (med2 + 1), i, dim);
     return med;
 }
 
-bool arePointsSame(int point1[], int point2[]) {
-    // Compare individual pointinate values
-    for (int i = 0; i < k; ++i)
-        if (point1[i] != point2[i])
-            return false;
-
-    return true;
-}
-
-
 // Driver program to test above functions
 int main() {
-    struct Node *root = NULL;
-    int points[][k] = {{3, 6}, {17, 15}, {13, 15}, {6, 12},
-                       {9, 1}, {2, 7}, {10, 19}};
-
+    Node *root = NULL;
+    int points[][k] = {{3, 6}, {17, 15}, {13, 15}, {6, 12}, {9, 1}, {2, 7}, {10, 19}};
     n = sizeof(points)/sizeof(points[0]);
     Node nodes[n];
     for(int i = 0; i < n; i++) {
         nodes[i] = (*newNode(points[i]));
     }
     root = buildKDtreeRec(nodes, n, 0, k);
+    print2D(root);
+
     return 0;
 }
